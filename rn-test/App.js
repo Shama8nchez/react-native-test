d7799e4e7a2d1281e300a32f57ac452a69e09898
Shama8nchez <shamanouski@gmail.com>
2024-08-11 22:05:31 +0300

    feat: added main page

--- rn-test/App.js
+++ rn-test/App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/Shama8nchez/react-native-test-server/posts"
+    )
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.form}>
+        <Text style={styles.title}>React Native App</Text>
+        <TextInput style={styles.textInput} placeholder="Title" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Body"
+          multiline
+          numberOfLines={3}
+        />
+        <Button
+          title="Create post"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
+      <View>
+        <Text style={styles.title}>List of posts:</Text>
+        {posts?.map((post) => (
+          <View style={styles.post} key={post.id}>
+            <Text style={styles.postTitle}>{post?.title}</Text>
+            <Text>{post?.body}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +45,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+
+  form: {
+    marginBottom: 15,
+  },
+
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+
+  textInput: {
+    padding: 5,
+    marginBottom: 10,
+    borderColor: "#111111",
+    borderStyle: "solid",
+    borderWidth: 1,
+  },
+
+  post: {
+    margin: 5,
+    padding: 5,
+    borderColor: "#444444",
+    borderStyle: "solid",
+    borderWidth: 1,
+  },
+
+  postTitle: {
+    fontWeight: 'bold',
   },
 });
